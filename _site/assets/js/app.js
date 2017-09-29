db4ebb50dc392ee50edf9d2fc94b75905e378fa3
Gareth Johns <info@garethjohnsdesign.com>
2017-09-29 11:19:09 +0100

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -24,3 +24,21 @@ imagesLoaded: true,
 // percentPosition: false,
 arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
+
+
+$('#video-gallery').lightGallery({
+    counter: false,
+    youtubePlayerParams: {
+        modestbranding: 1,
+        showinfo: 0,
+        rel: 0,
+        controls: 0
+    },
+    vimeoPlayerParams: {
+        autoplay: 0,
+        title : 0,
+        byline : 0,
+        portrait : 0,
+        color : 'FFFFFF'   
+    }
+});
